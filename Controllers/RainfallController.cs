0ac584722c0ad9c0139c223cf471c4fad74ea863
John <jcsay21@gmail.com>
2024-01-04 11:38:52 +0800

    Added the Rainfall Controller and Context

---
+++ Controllers/RainfallController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestRainfallMeasurement.Models;
+
+namespace TestRainfallMeasurement.Controllers
+{
+    /// <summary>
+    /// Rainfall Controller
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class RainfallController : ControllerBase
+    {
+        private readonly RainfallContext _rainfallContext;
+        
+       /// <summary>
+       /// Rainfall controller
+       /// </summary>
+       /// <param name="context">rainfall context</param>
+        public RainfallController(RainfallContext context)
+        {
+            _rainfallContext = context;
+        }
+
+        /// <summary>
+        /// Get the list of rainfall measurements
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "GetAllRainfall")]
+        public ActionResult<RainModel> GetAll()
+        {
+            return _rainfallContext.Rains.First();
+        }
+    }
+}
