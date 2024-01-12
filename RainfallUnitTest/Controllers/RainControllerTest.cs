b567ea52e667eead93ae7d466df7893b8ff4c41c
John <jcsay21@gmail.com>
2024-01-12 11:52:05 +0800

    Fixed the Solution projects.

---
+++ RainfallUnitTest/Controllers/RainControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RainfallUnitTest.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestRainfallMeasurement.Controllers;
+using TestRainfallMeasurement.Models;
+using TestRainfallMeasurement.Services;
+
+namespace RainfallUnitTest.Controllers
+{
+    public class RainControllerTest
+    {
+        private readonly RainfallController _controller;
+        private readonly IRainfallService _service;
+
+        /// <summary>
+        /// FID Contoller Test
+        /// </summary>
+        public RainControllerTest()
+        {
+            _service = new RainServiceTest();
+            _controller = new RainfallController(_service);
+        }
+
+        [Fact]
+        public void GetDelayedFlights_WhenCalled_ReturnsRightItem()
+        {
+            RequestModel requestModel = new RequestModel();
+            requestModel.Limit = 2;
+            // Act
+            var items = _controller.GetRainfallById("") as OkObjectResult;
+            // Assert
+            var itemsEq = Assert.IsType<List<RainItemModel>>(items.Value);
+            Assert.Equal(2, itemsEq.Count);
+        }
+    }
+}
---
+++ RainfallUnitTest/Services/RainServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using TestRainfallMeasurement.Models;
+using TestRainfallMeasurement.Services;
+
+namespace RainfallUnitTest.Services
+{
+    public class RainServiceTest : IRainfallService
+    {
+        private readonly List<RainItemModel> _rainItems;
+        public RainServiceTest()
+        {
+            _rainItems = new List<RainItemModel>()
+            {
+                new RainItemModel() { Id = "http://environment.data.gov.uk/flood-monitoring/data/readings/52203-rainfall-tipping_bucket_raingauge-t-15_min-mm/2024-01-11T12-00-00Z" ,
+                        Date = Convert.ToDateTime("2024-01-11T12:00:00Z") ,
+                        Measure = "http://environment.data.gov.uk/flood-monitoring/id/measures/52203-rainfall-tipping_bucket_raingauge-t-15_min-mm" ,
+                        Value = 0 },
+                new RainItemModel() { Id = "http://environment.data.gov.uk/flood-monitoring/data/readings/52203-rainfall-tipping_bucket_raingauge-t-15_min-mm/2024-01-11T11-45-00Z" ,
+                        Date = Convert.ToDateTime("2024-01-11T11:45:00Z") ,
+                        Measure = "http://environment.data.gov.uk/flood-monitoring/id/measures/52203-rainfall-tipping_bucket_raingauge-t-15_min-mm" ,
+                        Value = 0 }
+            };
+        }
+
+        /// <summary>
+        /// GetRainfallById
+        /// </summary>
+        /// <param name="request">rainfall params</param>
+        /// <returns>Rainfall list</returns>
+        public List<RainItemModel> GetRainfallById(RequestModel request)
+        {
+            return _rainItems.Select;
+        }
+    }
+}
